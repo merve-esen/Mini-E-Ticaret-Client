ac2bcde9c5cfa6545cd8fa59b226aa9cc5a343f4
Merve Esen <merveesen834@gmail.com>
2023-03-01 16:28:23 +0300

    feat: add multiple layout infrastructure

--- ETicaretClient/src/app/admin/components/product/product.module.ts
+++ ETicaretClient/src/app/admin/components/product/product.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,10 @@ import { ProductComponent } from './product.component';
     ProductComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: ProductComponent }
+    ])
   ]
 })
 export class ProductModule { }
