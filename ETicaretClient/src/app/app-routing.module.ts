ff0cc7e75fd6afebfb1d62cb7325f634ec221dd0
Merve Esen <merveesen834@gmail.com>
2023-03-02 10:01:26 +0300

    feat: add multiple layout infrastructure

--- ETicaretClient/src/app/app-routing.module.ts
+++ ETicaretClient/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "admin", component: LayoutComponent, children: [
+      { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
+      { path: "products", loadChildren: () => import("./admin/components/product/product.module").then(module => module.ProductModule) },
+      { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) }
+    ]
+  },
+  { path: "", component: HomeComponent },
+  { path: "products", loadChildren: () => import("./ui/components/product/product.module").then(module => module.ProductModule) },
+  { path: "basket", loadChildren: () => import("./ui/components/basket/basket.module").then(module => module.BasketModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
