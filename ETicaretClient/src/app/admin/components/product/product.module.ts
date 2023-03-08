da9520ef27317a74444976bcec0c386407c10610
Merve Esen <merveesen834@gmail.com>
2023-03-08 15:12:21 +0300

    feat: add delete directive

--- ETicaretClient/src/app/admin/components/product/product.module.ts
+++ ETicaretClient/src/app/admin/components/product/product.module.ts
@@ -10,12 +10,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
 
 @NgModule({
   declarations: [
     ProductComponent,
     CreateComponent,
-    ListComponent
+    ListComponent,
+    DeleteDirective
   ],
   imports: [
     CommonModule,
