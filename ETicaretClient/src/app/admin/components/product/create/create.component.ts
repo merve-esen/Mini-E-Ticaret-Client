4ed0207c254be0076671e6cb29c0d4d8126d5b0c
Merve Esen <merveesen834@gmail.com>
2023-03-08 08:59:00 +0300

    refactor: add error handling to product create method

--- ETicaretClient/src/app/admin/components/product/create/create.component.ts
+++ ETicaretClient/src/app/admin/components/product/create/create.component.ts
@@ -34,6 +34,13 @@ export class CreateComponent extends BaseComponent implements OnInit {
         messageType: MessageType.Success,
         position: Position.TopRight
       });
+    }, errorMessage => {
+      this.alertify.message(errorMessage,
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        });
     });
   }
 }
--- ETicaretClient/src/app/services/common/models/product.service.ts
+++ ETicaretClient/src/app/services/common/models/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateProduct } from 'src/app/contracts/product/create-product';
 import { HttpClientService } from '../http-client.service';
@@ -8,12 +9,21 @@ import { HttpClientService } from '../http-client.service';
 export class ProductService {
   constructor(private httpClientService: HttpClientService) { }
 
-  create(product: CreateProduct, successCallBack?: () => void) {
+  create(product: CreateProduct, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void) {
     this.httpClientService.post({
       controller: "products"
     }, product)
       .subscribe(result => {
         successCallBack();
+      }, (errorResponse: HttpErrorResponse) => {
+        const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error;
+        let message = "";
+        _error.forEach((v, index) => {
+          v.value.forEach((_v, _index) => {
+            message += `${_v}<br>`;
+          });
+        });
+        errorCallBack(message);
       });
   }
 }
